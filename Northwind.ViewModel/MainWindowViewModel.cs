8e4b638a8f68d08b3139fa3ca186f4f8f194e62b
Helena Coppieters <hcoppieters@hotmail.com>
2019-12-30 18:15:39 +0100

    Add project files.

---
+++ Northwind.ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Northwind.Data;
+using System.Data.Entity;
+
+using System.Linq;
+
+namespace Northwind.ViewModel
+{
+
+    public class MainWindowViewModel
+    {
+        private IList<Customer> _customers;
+        public IList<Customer> Customers {
+            get {
+                if (_customers is null)
+                {
+                    GetCustomers();
+                }
+                return _customers;
+            }
+        }
+        private void GetCustomers()
+        {
+            _customers = new northwindEntities().Customers.ToList();
+            int count = _customers.Count;
+        }
+    }
+}
